c160ae0fc5637c7f910cf9032e11956f6b60daab
KomfortKimko <komfortkimk@gmail.com>
2024-07-02 08:17:58 +0300

    Array-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(drivers, driverName) {
+    return drivers.filter(driver => driver.name === driverName);
+}
